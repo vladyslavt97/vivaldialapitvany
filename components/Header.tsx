d3d95305c4e11f8ce1fe6cef4b1a5ebc6dc2dfba
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-08-28 17:50:36 +0200

    animation of the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import Image from 'next/image'
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <div className='text-center mt-5 text-2xl font-extrabold text-black'>        
-        <h1>Vivaldi Alapítvány</h1>
+    <div className='w-full h-20'>
+      <motion.div 
+          initial={{opacity:0, scale:0.9}}
+          animate={{opacity:1, scale:1}}
+          transition={{duration:5}}>
+      <Image src="/viv.jpeg" width={1000} alt="some" height={1000} className=' object-cover absolute -z-10 opacity-5'/>
+      </motion.div>
+      <div className='text-center py-7 text-2xl font-extrabold text-black bg-green-800/30'>    
+
+          <motion.h1 
+          initial={{opacity:0, scale:0.9}}
+          animate={{opacity:1, scale:1}}
+          transition={{duration:5}}
+          >Vivaldi Alapítvány</motion.h1>
+      </div>
     </div>
   )
 }
