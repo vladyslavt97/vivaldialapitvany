4a5f4eacb6777f72727a4c0e6538e3b2df004266
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-09-02 12:05:15 +0200

    gallery and packages

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,5 +1,11 @@
 import { motion } from "framer-motion";
 import { useStore } from "@/components/State";
+import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 type Props = {}
 interface languagesState {
@@ -15,6 +21,19 @@ export default function Gallery({}: Props) {
         animate={{opacity:1}}
         transition={{duration:2, delay:1}}
         className='text-center text-xl font-extrabold text-black'>{language === "hun" ? "Képtár" : "Gallery "}</motion.h1>
+
+        <Swiper
+        pagination={{
+          type: "progressbar",
+        }}
+        navigation={true}
+        className="z-0"
+      >
+          <SwiperSlide >
+              <Image width={200} height={200} src="/him.jpeg" alt="photo" className="mx-auto my-5 w-80 h-80 object-contain" priority={true}/>
+          </SwiperSlide>
+        
+      </Swiper>
 
     </div>
   )
