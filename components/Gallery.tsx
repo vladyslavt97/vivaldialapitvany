ff54361b19999957d64737d4df09051a1e6a05a2
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-09-02 11:24:45 +0200

    gallery language switch

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,15 +1,20 @@
 import { motion } from "framer-motion";
+import { useStore } from "@/components/State";
 
 type Props = {}
-
+interface languagesState {
+  language: string,
+}
 export default function Gallery({}: Props) {
+  const language = useStore((state: languagesState) => state.language);
+
   return (
     <div className='mt-20'>
         <motion.h1 
         initial={{opacity:0}}
         animate={{opacity:1}}
         transition={{duration:2, delay:1}}
-        className='text-center text-xl font-extrabold text-black'>Gallery</motion.h1>
+        className='text-center text-xl font-extrabold text-black'>{language === "hun" ? "Elérhetőség:" : "Gallery "}</motion.h1>
 
     </div>
   )
