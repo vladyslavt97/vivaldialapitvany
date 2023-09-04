a41da95b736a771c0ff7235124f0e44783fbef67
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-09-04 12:21:54 +0200

    text black

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer({}: Props) {
 
     const mystyle = "font-bold"
   return (
-    <div className='bg-green-800/30 w-screen px-5 pt-6 pb-4 sm:px-24 sm:py-5 relative bottom-0 text-white text-sm sm:text-md'>
+    <div className='bg-green-800/30 w-screen px-5 pt-6 pb-4 sm:px-24 sm:py-5 relative bottom-0 text-black text-sm sm:text-md'>
         {copied === true && <Copied/>}
         <h2 className='font-bold'>{language === "hun" ? "Az alapítvány folyamatosan fogadja a támogatásokat az alábbi címen:" : "Donations are welcome by bank transfer; please use the details below:"}</h2>
         <h2 className='mt-6'>{language === "hun" ? "Adószám:" : "Tax number:"} 
