db4d84b43ee1f2c1ce9deb290a48110fa64817d5
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-09-05 23:10:23 +0200

    priotity for viv.png

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header({}: Props) {
           animate={{opacity:[0, 1, 0.5], scale:1.15, y:-21}}
           transition={{duration:5}}
           className='sm:hidden '>
-          <Image src="/viv.png" width={1000} alt="some" height={1000} className=' absolute -z-10 opacity-30 inset-0 object-cover w-full h-22' />
+          <Image src="/viv.png" width={1000} alt="some" height={1000} className=' absolute -z-10 opacity-30 inset-0 object-cover w-full h-22' priority={true}/>
       </motion.div>
 
       <motion.div 
