cc2d8eba912bbed813eb010d0bba7e0a31487109
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-09-05 22:35:25 +0200

    gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { useEffect, useState } from "react";
 
 type Props = {}
 interface languagesState {
@@ -14,27 +15,40 @@ interface languagesState {
 export default function Gallery({}: Props) {
   const language = useStore((state: languagesState) => state.language);
 
+  const [arr, setArr] = useState<number[]>([]);
+
+  useEffect(() => {
+    const newArray = [];
+    for (let i = 1; i <= 9; i++) {
+      newArray.push(i);
+    }
+    setArr(newArray);
+  }, []); 
+
+  
   return (
     <div className='mt-20'>
-        <motion.h1 
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:2, delay:1}}
-        className='text-center text-xl font-extrabold text-black'>{language === "hun" ? "Képtár" : "Gallery "}</motion.h1>
+      <motion.h1 
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      transition={{duration:2, delay:1}}
+      className='text-center text-xl font-extrabold text-black'>{language === "hun" ? "Képtár" : "Gallery "}</motion.h1>
 
-        <Swiper
-        pagination={{
-          type: "progressbar",
-        }}
-        navigation={true}
-        className="z-0"
+      <Swiper
+      pagination={{
+        type: "progressbar",
+      }}
+      navigation={true}
+      className="z-0"
       >
-          <SwiperSlide >
-              <Image width={200} height={200} src="/him.jpeg" alt="photo" className="mx-auto my-5 w-80 h-80 object-contain" priority={true}/>
-          </SwiperSlide>
-        
+        {arr.map((a,i)=>(
+          <div key={i}>
+            <SwiperSlide >
+              <Image width={200} height={200} src={`/gallery/${a}.jpg`} alt="photo" className="mx-auto my-5 w-80 h-80 object-contain" priority={true}/>
+            </SwiperSlide>
+          </div>
+        ))}
       </Swiper>
-
     </div>
   )
 }
