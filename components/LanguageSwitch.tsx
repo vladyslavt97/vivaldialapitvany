6a2dc9450af46e0d276350b08f8fac4956ab4121
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-09-16 12:50:16 +0100

    language switch corrected, motion, condition

--- components/LanguageSwitch.tsx
+++ components/LanguageSwitch.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect, useState } from "react";
 import { useStore } from "./State";
+import { easeInOut, motion } from "framer-motion";
 
 type Props = {}
 interface languagesState {
@@ -14,13 +15,14 @@ interface openState {
 }
 
 export default function LanguageSwitch({}: Props) {
+  const [loaded, setLoaded] = useState(false);
   const language = useStore((state: languagesState) => state.language);
 
 
   useEffect(() => {
     const browserLanguage = navigator.language;
     console.log(browserLanguage);
-    
+    setLoaded(true);
     if(browserLanguage.includes("hu")){
       setLanguage("hun");
     } else {
@@ -29,7 +31,7 @@ export default function LanguageSwitch({}: Props) {
   }, []);
 
   const styles = "bg-gradient-to-br from-blue-800/80 to-blue-800/30 rounded-full px-1 shadow-lg border border-blue-700";
-  const stylesselected = "bg-gradient-to-br from-blue-800/40 to-blue-800/10 rounded-full px-1 shadow-lg border border-blue-700";
+  const stylesselected = "bg-gradient-to-br from-blue-800/40 to-blue-800/0 rounded-full px-1 shadow-lg border border-blue-700";
   const engBio = () => {
     setLanguage('eng');
   }
@@ -43,10 +45,15 @@ export default function LanguageSwitch({}: Props) {
 
   return (
     <div className={`absolute left-1 md:left-8 top-1 sm:top-5 ${open ? "z-10" : "z-50"}`}>
-            <div className="flex flex-row sm:flex-col md:flex-row gap-1 md:gap-3 text-white font-extralight text-sm">
+      {loaded && 
+            <motion.div 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration:1, ease: "easeIn"}}
+            className="flex flex-row sm:flex-col md:flex-row gap-1 md:gap-3 text-white font-extralight text-sm">
                 <button onClick={hunBio} className={language === "hun" ? styles : stylesselected}>HUN</button>
                 <button onClick={engBio} className={language === "eng" ? styles : stylesselected}>ENG</button>
-            </div>
+            </motion.div>}
     </div>
   )
 }
