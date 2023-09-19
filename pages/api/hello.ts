0dfd9a4b62d99c5a04d002ea18c34d9b9778b5c1
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-09-19 10:15:50 +0100

    cookie-next

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import { setCookie, getCookie, hasCookie } from 'cookies-next';
 type Data = {
   name: string
 }
@@ -9,5 +8,19 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const options: any = { req, res };
+
+
+  //if there is cookie = do nothing
+  if(hasCookie('id', options)){
+    console.log('there is a cookie ? ', hasCookie('id', options));
+    console.log('which one: ', getCookie('id', options));
+
+  } else {
+    //else set a cookie 
+    setCookie('id', 'formidable', options);
+    //and update the mongodb
+  }
+  
   res.status(200).json({ name: 'John Doe' })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Description from '../components/ProjectDescription'
 import Gallery from '@/components/Gallery'
 import { useStore } from "@/components/State";
 import DescriptionENG from '@/components/ProjectDescriptionENG';
+import { useEffect } from 'react';
 
 interface languagesState {
   language: string,
@@ -11,6 +12,21 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const language = useStore((state: languagesState) => state.language);
+  useEffect(()=>{
+        fetch('/api/hello', {
+            method: 'GET', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => 
+                response.json())
+            .then((data) => {
+            })
+            .catch((error) => {
+                console.error('Error caught:', error);
+            });
+    },[])
   return (
     <main
       className={`text-[14px] flex min-h-screen flex-col justify-center px-7 sm:px-10 py-7 md:px-24 bg-blue-900/10 ${inter.className}`}
