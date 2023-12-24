8049f13b001c803d7be3fac0fd2ff9acb9cd6d66
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-12-24 13:01:59 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,20 +13,21 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const language = useStore((state: languagesState) => state.language);
   useEffect(()=>{
-        fetch('/api/hello', {
-            method: 'GET', 
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((response) => 
-                response.json())
-            .then((data) => {
-            })
-            .catch((error) => {
-                console.error('Error caught:', error);
-            });
-    },[])
+    fetch('/api/hello', {
+      method: 'GET', 
+      headers: {
+          'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => 
+      response.json())
+    .then((data) => {
+    })
+    .catch((error) => {
+      console.error('Error caught:', error);
+    });
+  },[])
+  //test
   return (
     <main
       className={`text-[14px] flex min-h-screen flex-col justify-center px-7 sm:px-10 py-7 md:px-24 bg-blue-900/10 ${inter.className}`}
