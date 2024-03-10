5fd4b9db47938c51847334a3f3d4ab11a32bf7a1
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2024-03-10 21:48:57 +0100

    bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Gallery from "@/components/Gallery";
 import { useStore } from "@/components/State";
 import DescriptionENG from "@/components/ProjectDescriptionENG";
 import { useEffect } from "react";
-
+import Image from "next/image";
 interface languagesState {
     language: string;
 }
@@ -30,8 +30,18 @@ export default function Home() {
     //test
     return (
         <main
-            className={`text-[14px] flex min-h-screen flex-col justify-center px-7 sm:px-10 py-7 md:px-24 bg-green-900/10 ${inter.className}`}
+            className={`text-[14px] flex min-h-screen flex-col justify-center px-7 sm:px-10 py-7 md:px-24 bg-mainbg bg-contain bg-no-repeat bg-fixed bg-center sm:bg-none sm:bg-green-900/10 ${inter.className}`}
         >
+            {/* <div className="absolute">
+                <Image
+                    src="/Vivaldi_back.png"
+                    width={1000}
+                    alt="some"
+                    height={1000}
+                    loading="eager"
+                    className="sticky top-20 left-0 w-screen -z-10"
+                />
+            </div> */}
             {language === "hun" ? <Description /> : <DescriptionENG />}
             <Gallery />
         </main>
