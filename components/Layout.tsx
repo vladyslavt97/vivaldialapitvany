e8e15d10f6724f3d70f7873b4bd522021e084530
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2024-03-19 22:28:30 +0100

    layout formatting

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,23 +1,29 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Header from "./Header";
-import Footer from './Footer';
+import Footer from "./Footer";
 
 interface LayoutProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
-export default function Layout({children}: LayoutProps) {
-  return (
-    <div>
-        <Head>
-        <title>Vivaldi Alapítvány</title>
-        <meta name="description" content="A Vivaldi Alapítvány célja kulturális programok szervezése, művészeti, kulturális nevelés, oktatás és a támogatottak művészeti pályájának elősegítése, a magyar szellemi, művészeti értékek és hagyományok feltárása és megismertetése." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-    </div>
-  )
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <div>
+            <Head>
+                <title>Vivaldi Alapítvány</title>
+                <meta
+                    name="description"
+                    content="A Vivaldi Alapítvány célja kulturális programok szervezése, művészeti, kulturális nevelés, oktatás és a támogatottak művészeti pályájának elősegítése, a magyar szellemi, művészeti értékek és hagyományok feltárása és megismertetése."
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </div>
+    );
 }
